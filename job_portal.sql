be63e103800f0c963d96c3b8d9e2e8cf317926e5
Sarah Fathi <sarah.fathi@epitech.eu>
2024-10-08 14:36:48 +0200

    test

--- job_portal.sql
+++ job_portal.sql
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `advertisements`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `advertisements` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `company_id` int(11) DEFAULT NULL,
