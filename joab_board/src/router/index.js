9e217204d3ef6df7e0d14022b5e7e5c9e93ab139
Sarah Fathi <sarah.fathi@epitech.eu>
2024-10-11 10:30:07 +0200

    plus rien ne marche

---
+++ joab_board/src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'; // Ensure this line is correct
+import Home from '@/components/Home.vue';
+import LoginPage from '@/components/LoginPage.vue'; // Update to match your new filename
+import JobDetail from '@/components/JobDetail.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'HomePage',
+    component: Home,
+  },
+  {
+    path: '/login',
+    name: 'LoginPage',
+    component: LoginPage,
+  },
+  {
+    path: '/job/:id',
+    name: 'JobDetail',
+    component: JobDetail,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
