1ed8b7a032f220e0ff536b136d98b13f019e86b1
Sarah Fathi <sarah.fathi@epitech.eu>
2024-10-17 12:28:14 +0200

    no job_board

--- joab_board/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'; // Import createApp
-import App from './App.vue';
-import router from './router';
-
-const app = createApp(App); // Create the Vue app
-app.use(router); // Use the router
-app.mount('#app'); // Mount the app
